f6ebb8a24fc60dfcd446a88275749c23fd2d15b6
Thenewclosedbitter <76114338+Thenewclosedbittter@users.noreply.github.com>
2026-04-25 21:58:16 +0000

    Add button position and size configurations

--- Src/ui/config.py
+++ Src/ui/config.py
@@ -6,4 +6,23 @@ Colours = {
     "BLUE" : (0, 0, 255)
 }
 
+button_position = {
+	'b_saves' : (0, 300)
+}
+
+# Menu configs: these configs for the menu
+# Size of buttons that go back after clicking start 
+saves_button_size = {
+	"width" : 500, 
+	"height" : 100
+}
+
+
+bible_versions = ["King James Bible", "World English Bible (WEB)", "Young's Literal Translation (YLT)", "Bible in Basic English"]
+text_col = "BLACK"
+# Button y layouts 
+y = -150
+space = 140
+button_y = [y + i * space for i in range(4)]
+    
 
